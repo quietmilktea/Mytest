c9ab30d3675c8c4c8775694c7402a4b277f998f0
quietmilktea <1502191H@student.tp.edu.sg>
2017-06-26 20:48:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -162,53 +162,6 @@ def deletenodes():
 
 	res = requests.delete(url, headers=headers, verify=False)
 	return res.text
-# SKUs
-@app.route('/rackhd/skus/read', methods=['GET'])
-def readskus():
-	url = "https://localhost:8443/api/current/skus"
-	token = request.headers.get('token')
-
-	headers = {
-		"Content-Type": "application/json",
-		"Authorization": "JWT " + token
-	}
-
-	res = requests.get(url, headers=headers, verify=False)
-
-	return res.text
-
-@app.route('/rackhd/skus/create', methods=['PUT'])
-def createskus():
-	url = "https://localhost:8443/api/current/skus"
-	token = request.headers.get('token')
-	headers = {
-		"Content-Type": "application/json",
-		"Authorization": "JWT " + token
-	}
-
-	name = request.json['name']
-	path = request.json['path']
-	contains = request.json['contains']
-	path = request.json['path']
-
-	data = '{}'
-
-	res = requests.put(url, headers=headers, data=data, verify=False)
-	print res.text
-
-@app.route('/rackhd/skus/delete', methods=['DELETE'])
-def deleteskus():
-	skuid = request.json['skuid']
-	url = "https://localhost:8443/api/current/skus/" + skuid
-
-	token = request.headers.get('token')
-
-	headers = {
-		"Content-Type": "application/json",
-		"Authorization": "JWT " + token
-	}
-
-	res = requests.delete(url, headers=headers, verify=False)
 
 
 if __name__ == '__main__':
